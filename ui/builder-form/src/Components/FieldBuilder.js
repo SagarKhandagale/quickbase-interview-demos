fa6ceaa83d871633660d90aa6252401329e24afb
SagarKhandagale <khandagale.s@northeastern.edu>
2022-10-17 16:25:03 -0400

    completed logic for textarea validation

--- ui/builder-form/src/Components/FieldBuilder.js
+++ ui/builder-form/src/Components/FieldBuilder.js
@@ -8,27 +8,51 @@ const FieldBuilder = () => {
     const [reqVal, setReqVal] = useState(true);
     const [defVal, setDefVal] = useState('');
     const [choices, setChoices] = useState('');
-    const [order, setOrder] = useState('select');
+    const [orderAlpha, setOrderAlpha] = useState(false);
+    
 
     const postSubmit = (e) => {
         e.preventDefault();
-        // var label = document.getElementById('label').value;
-        // var type = document.getElementById('type').value;
-        // var choices = document.getElementById('choices').value;
-        // var defaultVal = document.getElementById('default').value;
-        // var order = document.getElementById('order').value;
-    
+
+        checkDefInChoices();
+
+        var choicesArr = document.getElementById('choice-select').value.split('\n');
+
+        var data = {
+            "label": label,
+            "required": type,
+            "choices": choicesArr,
+            "displayAlpha": orderAlpha,
+            "default": defVal
+        }
+
+        console.log('FieldBuilder submitted data');
+        console.log(data);
+
         axios.post('http://www.mocky.io/v2/566061f21200008e3aabd919', {
-            'label':label,
-            'type':type,
-            'choices':JSON.stringify(choices),
-            'default':defVal,
-            'order':order
+            data
         }).then(result => {
-            console.log(result)
+            console.log(result);
         }).catch(err => {
-            console.log(err)
+            console.log(err);
         });
+    }
+
+    const checkDefInChoices = () => {
+        var choicesText = document.getElementById('choice-select')
+        var choicesArr = choicesText.value.split('\n');
+        
+        if(!choicesArr.includes(defVal)) {
+            if (choicesArr.length >= 5) {
+                alert('Default choice cannot be added because choices list is full');
+            }
+            else {
+                choicesArr.push(defVal);
+                document.getElementById('choice-select').value = choicesArr.join('\n');
+                setChoices(choicesText.value);
+                alert('Default choice has been added to the choices list');
+            }
+        }
     }
 
     const handleLabelChange = (event) => {
@@ -48,19 +72,63 @@ const FieldBuilder = () => {
     }
 
     const handleChoices = (event) => {
-        setChoices(event.target.value);
+
+        // get the textarea
+        const choicesText = document.getElementById('choice-select');
+        var totalLines = document.getElementById('choice-select').value.split('\n');
+        var lineNum = choicesText.value.substr(0, choicesText.selectionStart).split("\n").length;
+        var linesTillCursor = choicesText.value.substr(0, choicesText.selectionStart).split("\n");
+        var choiceAdded = linesTillCursor[lineNum - 1];
+        linesTillCursor.splice(lineNum - 1, 1);
+
+        if (event.key === 'Enter') {
+            // logic for textarea validations
+            // get the line number where you are
+            // check the value at that line 
+            // if value is duplicate remove that line and set the cursor on same line
+            // if not duplicate then check if total lines are 50 or grater 
+            // if greater then remove that line
+
+            // if entered value is duplicate
+            if (linesTillCursor.includes(choiceAdded)) {
+                totalLines.splice(lineNum - 1, 1);
+                totalLines = totalLines.filter((val) => { return val !== '' });
+                choicesText.value = totalLines.join('\n');
+                alert('Choice already present');
+            }
+            else if (totalLines.length > 5) {
+
+                // if cursor is at last line then only remove that value else keep the value and just show the alert
+                if (totalLines.length === lineNum) {
+                    totalLines.splice(lineNum - 1, 1);
+                }
+
+                totalLines = totalLines.filter((val) => { return val !== '' });
+                choicesText.value = totalLines.join('\n');
+                alert('Choices cannot be more than 5');
+            }
+
+            // if order alphabetically is selected in order dropdown then to sort values at runtime
+            if (orderAlpha) {
+                sortChoices(choicesText);
+            }
+
+            setChoices(choicesText.value);
+        }
     }
 
     const handleOrderChange = (event) => {
-        setOrder(event.target.value);
-        if (event.target.value === 'alphabetical') {
+        setOrderAlpha(!orderAlpha);
+        if (event.target.value) {
             const choicesText = document.getElementById('choice-select');
             sortChoices(choicesText);
         }
     }
 
     const sortChoices = (choices) => {
-        choices.value = choices.value.split('\n').sort().join('\n');
+        var choicesArr = choices.value.split('\n');
+        choicesArr = choicesArr.filter((val) => { return val !== '' });
+        choices.value = choicesArr.sort().join('\r\n');
     }
 
     return (
@@ -85,17 +153,14 @@ const FieldBuilder = () => {
 
                 <div id='choices'>
                     <label> Choices </label>
-                    <textarea style={{ resize: 'none', height: '100px', overflowY: 'scroll' }} id='choice-select' cols='30' rows='5' onChange={handleChoices}></textarea>
+                    <textarea style={{ resize: 'none', height: '100px', overflowY: 'scroll' }} id='choice-select' cols='30' rows='5' onKeyDown={handleChoices}></textarea>
                 </div>
 
                 <div id='order'>
                     <label>
                         Order:
-                        <select value={order} onChange={handleOrderChange}>
-                            <option value='select'>select</option>
-                            <option value='alphabetical'>Display choices in alphabetical</option>
-                            <option value='test'>test</option>
-                        </select>
+                        <input type='checkbox' name='order-alpha' checked={orderAlpha} onChange={handleOrderChange} />
+                        <label>Display choices in alphabetical</label>
                     </label>
                 </div>
 
